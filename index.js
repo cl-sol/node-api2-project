a31fabb25b0ceecffaf5f252cb738ff2c36fb9cd
Claudia Sol <Claudia@Claudias-MacBook-Pro.local>
2020-08-11 15:27:51 -0400

    mvp

---
+++ index.js
@@ -0,0 +1,14 @@
+// const express = require("express")
+// const postsRouter = require("./posts/posts-router")
+
+// const server = express()
+// server.use(express.json())
+// server.use(postsRouter)
+
+// server.get("/", (req, res) => {
+//     res.send(`
+//         <h2>Posts API<h2>
+//         <p>Welcome</p>
+//     `)
+// })
+
