1138532a9faf9f1ac2fe5b5ee16d05cc5e6e7306
Claudia Sol <Claudia@Claudias-MacBook-Pro.local>
2020-08-11 15:30:41 -0400

    forgot index

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-// const express = require("express")
-// const postsRouter = require("./posts/posts-router")
+const express = require("express")
+const postsRouter = require("./posts/posts-router")
 
-// const server = express()
-// server.use(express.json())
-// server.use(postsRouter)
+const server = express()
+server.use(express.json())
+server.use(postsRouter)
 
-// server.get("/", (req, res) => {
-//     res.send(`
-//         <h2>Posts API<h2>
-//         <p>Welcome</p>
-//     `)
-// })
+server.listen(3000, () => {
+    console.log("server running on port 3000")
+})
+
+server.get("/", (req, res) => {
+    res.send(`
+        <h2>Posts API<h2>
+        <p>Welcome</p>
+    `)
+})
 
