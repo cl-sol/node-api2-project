5b20bc9e82abaa945352f2123783f5a15b28407e
Claudia Sol <Claudia@Claudias-MacBook-Pro.local>
2020-08-16 22:21:15 -0400

    deploying to heroku

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+require("dotenv").config();
+
 const express = require("express")
 const postsRouter = require("./posts/posts-router")
 
 const server = express()
+
+const port = process.env.PORT || 3000;
 server.use(express.json())
 server.use(postsRouter)
 
-server.listen(3000, () => {
-    console.log("server running on port 3000")
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 })
 
 server.get("/", (req, res) => {
